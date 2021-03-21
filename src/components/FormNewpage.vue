ad6f40aa34794e86d413030ea69dc9d085a6a562
lulu <lucas.daval@edu.devinci.fr>
2021-03-21 12:57:53 +0100

    :art: Form new page css

--- src/components/FormNewpage.vue
+++ src/components/FormNewpage.vue
@@ -1,23 +1,31 @@
 <template>
     <form action="/" method="post">
-        <div>
+        <div id="formFirstPart">
             <ul>
                 <li>
                     <label for="title">Titre de la page</label>
-                    <input name="title" id="title" type="text">
+                    <input name="title" id="title" type="text" required>
                 </li>
-                
+
                 <li>
                     <label for="metaDesc">Meta Description</label>
-                    <input name="metaDesc" id="metaDesc" type="text">
+                    <input name="metaDesc" id="metaDesc" type="text" required>
                 </li>
 
                 <li>
                     <label for="metaTitle">Meta Title</label>
-                    <input name="metaTitle" id="metaTitle" type="text">
+                    <input name="metaTitle" id="metaTitle" type="text" required>
                 </li>
             </ul>
+
+            <div>
+                <label for="image">Ajouter une image</label>
+                <input type="" name="image" id="image">
+            </div>            
         </div>
+
+        <label for="postBody">Corps du post</label>
+        <textarea name="postBody" id="postBody" cols="100" rows="15"></textarea>
     </form>
 </template>
 
@@ -30,5 +38,24 @@ export default {
 <style scoped>
     *{
         list-style: none;
+    }
+
+    form{
+        margin: 8% 12%;
+        text-align: left;
+    }
+
+    ul{
+        width: 50%;
+    }
+
+    li{
+        display: flex;
+        justify-content: space-between;
+        margin: 3% 0;
+    }
+
+    #formFirstPart{
+        display: flex;
     }
 </style>
